336016cc51604e84d356c6d34e9488d4cb227280
Ossipoika <eero.alajarvi@icloud.com>
2022-05-15 16:25:08 +0300

    Ikoneita Androidille

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -2,5 +2,23 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'public/service-worker.js',
+      exclude: [
+        /_redirects/,
+        /\.map$/,
+        /manifest\.json$/
+      ],
+    },
+    background_color: '#ffffff',
+    themeColor: '#ffffff',
+    msTileColor: '#ffffff',
+    backgroundColor: '#ffffff',
+    theme_color: '#ffffff',
+    start_url: '/.',
+    display: 'standalone'
+  }
 })
